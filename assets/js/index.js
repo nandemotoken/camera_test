d1daccb7652bc6f06e30dca61eb73b5f8891d586
nandemotoken <nandemotoken@gmail.com>
2021-06-23 11:01:09 +0000

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,8 @@ const medias = {
 };
 
 const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 window.onload = () => {
 const promise = navigator.mediaDevices.getUserMedia(medias);
@@ -12,8 +14,17 @@ promise.then(successCallback).then(errorCallback);
 
 function successCallback(stream) {
   video.srcObject = stream;
+  requestAnimationFrame(draw);
 };
 
 function errorCallback(err) {
   console.log(err);
 };
+
+function draw() {
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.drawImage(video, 0, 0);
+
+  requestAnimationFrame(draw);
+}
